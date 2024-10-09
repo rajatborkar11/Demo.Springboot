48db83756955e09761af5df6d987c339fabedeca
rajatborkar145@gmail.com <rajatborkar145@gmail.com>
2024-10-09 16:02:13 +0530

    Initial commit

---
+++ src/main/java/com/example/demo/controller/DemoController.java
@@ -0,0 +1,19 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+
+public class DemoController {
+	
+	 @RequestMapping("/hello")
+	    @ResponseBody
+	    public String helloWorld()
+	    {
+	        return "Hello World!";
+	    }
+	}
+
+
